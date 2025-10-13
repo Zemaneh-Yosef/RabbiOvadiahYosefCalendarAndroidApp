b128e4ce0d447ee3c458cd608db5163cb76ce09e
NightScript <18664762+NightScript370@users.noreply.github.com>
2025-10-13 04:25:14 -0400

    Kaddish now features smaller text. Also, for stanza/choir portions of the siddur, implement helper functions

--- app/src/main/java/com/ej/rovadiahyosefcalendar/classes/SiddurKahalStringBuilder.java
+++ app/src/main/java/com/ej/rovadiahyosefcalendar/classes/SiddurKahalStringBuilder.java
@@ -4,23 +4,61 @@ import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.style.RelativeSizeSpan;
 
+import androidx.annotation.NonNull;
+
 public class SiddurKahalStringBuilder extends SpannableStringBuilder {
-	public SiddurKahalStringBuilder addRepeatedKahalChorusSmallText(String[] stanzas, String chorus, String stanzaPrefix) {
+	public SiddurKahalStringBuilder() {
+		super();
+	}
+
+	public SiddurKahalStringBuilder(SiddurKahalStringBuilder kaddishText) {
+		super(kaddishText);
+	}
+
+	public SiddurKahalStringBuilder addRepeatedKahalChorusSmallText(boolean inline, String[] stanzas, String chorus, String stanzaPrefix) {
 		for (String stanza: stanzas) {
-			super.append(stanzaPrefix + stanza);
+			super.append(stanzaPrefix).append(stanza);
+			this.appendSmallText(chorus);
 
-			int preChorusLength = super.length();
-			super.append(chorus);
-			super.setSpan(new RelativeSizeSpan(0.7f), preChorusLength, super.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-
-			super.append("\n");
+			if (!inline)
+				super.append("\n");
 		}
 
-		super.delete(super.length() - 1, super.length());
+		if (!inline)
+			super.delete(super.length() - 1, super.length());
 		return this;
+	}
+
+	public SiddurKahalStringBuilder addRepeatedKahalChorusSmallText(String[] stanzas, String chorus, String stanzaPrefix) {
+		return this.addRepeatedKahalChorusSmallText(false, stanzas, chorus, stanzaPrefix);
 	}
 
 	public SiddurKahalStringBuilder addRepeatedKahalChorusSmallText(String[] stanzas, String chorus) {
 		return this.addRepeatedKahalChorusSmallText(stanzas, chorus, "");
+	}
+
+	public SiddurKahalStringBuilder appendSmallText(String smallText) {
+		int preSmall = super.length();
+		super.append(smallText);
+		super.setSpan(new RelativeSizeSpan(0.7f), preSmall, super.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+		return this;
+	}
+
+	public SiddurKahalStringBuilder appendAmen() {
+		return appendSmallText(" [אמן] ");
+	}
+
+	@NonNull
+	@Override
+	public SiddurKahalStringBuilder append(char text) {
+		super.append(text);
+		return this;
+	}
+
+	@NonNull
+	public SiddurKahalStringBuilder append(String text) {
+		super.append(text);
+		return this;
 	}
 }
